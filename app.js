a81de805f1415efce3537c8fe6c88ce8e1330854
maryumsaleem <maryumangel3@gmail.com>
2020-07-23 21:52:10 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+//Task 1"
+// function accept(){
+// 	alert("your name is added");
+// }
+//Task 2:
+// function purchased(){
+// 	alert("Thanks for purchasing a phone from us");
+// }
+
+//Task 3:
+// function del(){
+// var del=document.getElementById('first').remove();
+// }
+// function one(){
+// 	var a=document.getElementById('second').remove();
+// }
+// function two(){
+// 	var b=document.getElementById('third').remove();
+// }
+// function three(){
+// 	var c=document.getElementById('fourth').remove();
+// }
+
+
+//Task 4:
+// function change(){
+// var mobile=document.getElementById('image')
+// 	mobile.src="mobile3.jpg"
+// }
+// function set(){
+// 	var change=document.getElementById('image')
+// 	change.src="mobile1.jpg"
+// }
+
+//Task 5:
+// function add(){
+// 	var increase=document.getElementById('inc').innerHTML;
+// 	increase++;
+// 	document.getElementById('inc').innerHTML =increase;
+// }
+// function sub(){
+// 	var decrease=document.getElementById('inc').innerHTML;
+// 	decrease--;
+// 	document.getElementById('inc').innerHTML =decrease;
+// }
+
+//chp 9 to 52
+//Task 1:
+// function add(){
+// 	var firstname=document.getElementById('name');
+// 	var second name=document.getElementById('secname');
+// 	var password=document.getElementById('pass');
+// 	var formfirstname=document.getElementById('secname');
+// 	var formsecond name=document.getElementById('sname');
+// 	var formpassword=document.getElementById('secpass');
+// 	formfirstname.innerHTML=firstname.innerHTML
+
+// }
+
+
+//Task 2:
+// function read(){
+// 	var text="Suppose in your webpage there is content area in which you have entered your item details, but user can only see some details on first look. When user clicks on Read more button full detail of that particular item will be displayed.In previous assignment you have created a tabular data using javascript. Let modify that. Create a form which takes student details and show each student detail in table. Each row of table must contain a delete button and an edit button. On click on delete button entire row should be deleted. On click on edit button a hidden form will appear with the values of that row."
+// 	var more =document.getElementById('para');
+// 	 more.innerHTML=text  
+
+// 	}
+
+//Task 3:
+function del(){
+var del=document.getElementById('first').remove();
+}
+function one(){
+	var a=document.getElementById('second').remove();
+}
+function two(){
+	var b=document.getElementById('third').remove();
+}
+function three(){
+	var c=document.getElementById('fourth').remove();
+}
+function edit(){
+	var d=document.getElementById('first')
+
+}
